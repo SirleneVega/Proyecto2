7bb71320fcc2734512b7ab2f4fd1cceae9ee69f8
SirleneVega <51767766+SirleneVega@users.noreply.github.com>
2020-07-05 18:52:26 -0600

    FrmPrincipal

--- AppProyectoII/AppProyectoII/FrmPrincipal.Designer.cs
+++ AppProyectoII/AppProyectoII/FrmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AppProyectoII
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
--- AppProyectoII/AppProyectoII/Program.cs
+++ AppProyectoII/AppProyectoII/Program.cs
@@ -16,7 +16,7 @@ namespace AppProyectoII
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
